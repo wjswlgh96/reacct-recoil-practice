98883f8219b9d429b910662fdf9de829a3d5675b
JiHo Jeon <wjswlgh96@naver.com>
2024-10-23 16:39:23 +0900

    react-hook-form 사용법

---
+++ src/constant/regExp.ts
@@ -0,0 +1,1 @@
+export const EMAIL_EXP = /^[A-Za-z0-9._%+-]+@naver.com$/;
---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2c3e50",
+  textColor: "#ecf0f1",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  textColor: "#111",
+};
